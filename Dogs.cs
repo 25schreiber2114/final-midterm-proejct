35bbaeae29a3dfab4a16e11e442dd7e18d7ca472
25schreiber2114 <120128631+25schreiber2114@users.noreply.github.com>
2022-12-19 13:24:32 -0500

    Initial commit

---
+++ Dogs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Program Name: Client List
+ * Programmer: Luke Schreiber
+ * Date: 12/19/2022
+ *
+ * program does : Creates a list of clients for your law firm
+ * with specific age and names.
+ */
+namespace SimpleProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>() { "kane", "kevin", "kale", "kellen" };
+            List<int> ages = new List<int>() { 32, 34, 91, 28 };
+
+            Person person;
+            for (int i = 0; i < names.Count; i++)
+            {
+                person = new Person(names[i], ages[i]);
+                Console.WriteLine("Name: {0}, Age: {1}", person.Name, person.Age);
+            }
+        }
+    }
+
+}
